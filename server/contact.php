3edf4620414c77220a581a8994aa435bf4e136fb
witzawitz <witzawitz@gmail.com>
2020-02-17 23:22:49 +0300

    put contacts to csv

--- server/contact.php
+++ server/contact.php
@@ -3,14 +3,22 @@
 	/*if ($_SERVER[ 'HTTP_HOST' ] != $_SERVER[ 'SERVER_NAME' ]) {
 		exit();
 	}*/
-	
-	$today = date("d-m-Y, G:i");
+
+	$today = date("Y-m-d H:i:s");
 	foreach ([ 'name', 'email', 'org', 'position', 'phone' ] as $key) {
 		$$key = isset($_POST[ $key ]) ? $_POST[ $key ] : null;
 	}
 
-	$f = fopen("data.txt", "aw");
-	fwrite($f, "\nЗаявка от ".$today.". ФИО: ".$name.". Email: ".$email.". Организация: ".$org.". Должность: ".$position.". Телефон: ".$phone);
+	$f = fopen("data.csv", "aw");
+	fputcsv($f, [
+		$today,
+		$name,
+		$email,
+		$org,
+		$position,
+		$phone
+	]);
+//	fwrite($f, "\nЗаявка от ".$today.". ФИО: ".$name.". Email: ".$email.". Организация: ".$org.". Должность: ".$position.". Телефон: ".$phone);
 	echo json_encode([ 'status' => 200, 'message' => 'Success' ]);
 	fclose($f);
 
