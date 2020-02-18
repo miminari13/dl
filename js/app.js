cf73bb87ba20b63268422f857b5b88729656d70f
miminari13 <miminari13@gmail.com>
2020-02-18 21:19:52 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -1,17 +1,63 @@
 $(document).ready(function() {
 	$('#apply-phone').mask("+7 (999) 999-99-99");
 
-	 $('#submit').click(function(e) {
+	function widget(prefillSurname, prefillName, prefillEmail, prefillOrg, prefillPos) {
+		var datawidget = '<script type="text/javascript" async="async" defer="defer" charset="UTF-8" ' +
+		    'src="https://timepad.ru/js/tpwf/loader/min/loader.js" ' +
+		    'data-timepad-customized="46980" data-twf2s-event--id="1256981" ' +
+		    'data-timepad-widget-v2="event_register">' +
+		      '(function() {' +
+		        'return {' +
+		          '"prefill": {' +
+		            '"attendees": [' +
+		              '{' +
+		                '"surname": "' + prefillSurname + '",' +
+		                '"name": "' + prefillName + '",' +
+		                '"mail": "' + prefillEmail + '",' +
+		                '"5050684": "' + prefillOrg + '",' +
+		                '"5050685": "' + prefillPos + '",' +
+		              '}' +
+		            ']' +
+		          '}' +
+		        '};' +
+		      '})();' +
+		    '</script>';
+
+		$('.timepad-widget').append(datawidget);
+
+		$('.conference__form').addClass('d-none');
+		$('.timepad-widget').removeClass('d-none');
+	};
+	function formFilled() { 
+	    if (localStorage.getItem('5sgDNK_f') == 'true') {
+
+	    	var surnameData = localStorage.getItem('5sgDNK_s'),
+	    		nameData = localStorage.getItem('5sgDNK_n'),
+	            emailData = localStorage.getItem('5sgDNK_e'),
+	    		orgData = localStorage.getItem('5sgDNK_o'),
+	            posData = localStorage.getItem('5sgDNK_p');
+	      
+			if (surnameData !== null && nameData !== null && emailData !== null && orgData !== null && posData !== null) {
+				widget(surnameData, nameData, emailData, orgData, posData);
+			} else {
+				widget("", "", "");
+			}     
+		};
+	};
+	formFilled();
+
+	$('#submit').click(function(e) {
 
     	e.preventDefault();
         var name      = $('#apply-name').val(),
+        	surname   = $('#apply-surname').val(),
         	email     = $('#apply-email').val(),
         	org       = $('#apply-org').val(),
         	position  = $('#apply-position').val(),
             phone     = $('#apply-phone').val()
             urlAdr    = './server/contact.php';
 
-        if (!name || !email || !org || !position || !phone) {
+        if (!name || !surname || !email || !org || !position || !phone) {
 
         	$('.alert-danger').removeClass('d-none');
 
@@ -21,6 +67,7 @@ $(document).ready(function() {
 
         	var data = {
 	            name : name,
+	            surname : surname,
 	            email : email,
 	            org : org,
 	            position : position,
@@ -33,10 +80,25 @@ $(document).ready(function() {
 	            data     : data,
 	            dataType : "JSON",
 	            success: function(xhr) {
-	            	console.log(xhr);
+
 	            	if (xhr.status == '200') {
 	            		$('.conference__form').addClass('d-none');
 	            		$('.alert-success').removeClass('d-none');
+
+	            		localStorage.setItem('5sgDNK_n', name);
+	            		localStorage.setItem('5sgDNK_s', surname);
+				        localStorage.setItem('5sgDNK_e', email);
+				        localStorage.setItem('5sgDNK_o', org);
+				        localStorage.setItem('5sgDNK_p', position);
+				        localStorage.setItem('5sgDNK_f', 'true');
+
+				        widget(
+				        	localStorage.getItem('5sgDNK_n'), 
+				        	localStorage.getItem('5sgDNK_s'), 
+				        	localStorage.getItem('5sgDNK_e'), 
+				        	localStorage.getItem('5sgDNK_o'), 
+				        	localStorage.getItem('5sgDNK_p'));
+
 	            	} else if (xhr.status == '422') {
 	            		$('.alert-danger').removeClass('d-none');
 	            	}
--- server/contact.php
+++ server/contact.php
@@ -5,7 +5,7 @@
 	}*/
 
 	$today = date("Y-m-d H:i:s");
-	foreach ([ 'name', 'email', 'org', 'position', 'phone' ] as $key) {
+	foreach ([ 'name', 'surname', 'email', 'org', 'position', 'phone' ] as $key) {
 		$$key = isset($_POST[ $key ]) ? $_POST[ $key ] : null;
 	}
 
@@ -13,12 +13,13 @@
 	fputcsv($f, [
 		$today,
 		$name,
+		$surname,
 		$email,
 		$org,
 		$position,
 		$phone
 	]);
-//	fwrite($f, "\nЗаявка от ".$today.". ФИО: ".$name.". Email: ".$email.". Организация: ".$org.". Должность: ".$position.". Телефон: ".$phone);
+
 	echo json_encode([ 'status' => 200, 'message' => 'Success' ]);
 	fclose($f);
 
