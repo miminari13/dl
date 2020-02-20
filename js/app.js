c86947e102a764e65f423f283db0e52ca18595ce
miminari13 <miminari13@gmail.com>
2020-02-20 22:50:08 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -1,54 +1,21 @@
 $(document).ready(function() {
 	$('#apply-phone').mask("+7 (999) 999-99-99");
 
-		$('#submit').click(function(e) {
+	var menu = $('.pre-header__menu');
+	$('.js-show-menu').click(function(){
+		if (!menu.hasClass('open')) {
+			menu.addClass('open');
+		} else {
+			menu.removeClass('open');
+		}
+	});
 
-    	e.preventDefault();
-        var name      = $('#apply-name').val(),
-        	surname   = $('#apply-surname').val(),
-        	email     = $('#apply-email').val(),
-        	org       = $('#apply-org').val(),
-        	position  = $('#apply-position').val(),
-            phone     = $('#apply-phone').val()
-            urlAdr    = './server/contact.php';
 
-        if (!name || !surname || !email || !org || !position || !phone) {
+	$('.js-show-programm').click(function(){
+		$('.programm__fulllist').addClass('open');
+		$('.programm__pre-title--spec').removeClass('programm__pre-title--last');
+		$('.programm__info--spec').removeClass('programm__info--last');
 
-        	$('.alert-danger').removeClass('d-none');
-
-        } else {
-
-        	$('.alert-danger').addClass('d-none');
-
-        	var data = {
-	            name : name,
-	            surname : surname,
-	            email : email,
-	            org : org,
-	            position : position,
-	            phone : phone
-	        };
-
-	        $.ajax({
-	            type     : "POST",
-	            url      : urlAdr,
-	            data     : data,
-	            dataType : "JSON",
-	            success: function(xhr) {
-
-	            	if (xhr.status == '200') {
-	            		$('.conference__form').addClass('d-none');
-	            		$('.alert-success').removeClass('d-none');
-
-	            		if (localStorage.getItem('5sgDNK_n')) {
-	            			localStorage.clear();
-	            		}
-
-	            	} else if (xhr.status == '422') {
-	            		$('.alert-danger').removeClass('d-none');
-	            	}
-		        }
-	        });
-        }
-    });
+		$(this).hide();
+	});
 });
